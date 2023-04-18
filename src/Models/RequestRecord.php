5d52c6da79bd8f1eea8959c3cc42cf8c73dfc341
ryuujin <mirchaemanuel@gmail.com>
2023-04-19 01:06:38 +0200

    fixed phpstan

--- src/Models/RequestRecord.php
+++ src/Models/RequestRecord.php
@@ -2,9 +2,26 @@
 
 namespace Illegal\RequestAudit\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $url
+ * @property string $method
+ * @property string $ip
+ * @property string $forwarded_ip
+ * @property string $cloudflare_ip
+ * @property array $headers
+ * @property string $user_agent
+ * @property array $content
+ * @property array $body
+ * @property array $server_params
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @method static RequestRecord create(array $attributes)
+ */
 class RequestRecord extends Model
 {
     use HasFactory;
